6dba04ac7ee356c78abebb4618a86c9147efe617
Bruno Santana <santana.b91@gmail.com>
2022-07-22 21:39:40 -0300

    Primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product produto = new Product();
+		
+		System.out.println("Adicione a informação do produto: ");
+		System.out.print("Nome: ");
+		produto.nome = sc.nextLine();
+		System.out.print("Preço: ");
+		produto.preco = sc.nextDouble();
+		System.out.print("Quantidade em estoque: ");
+		produto.quantidade = sc.nextInt();
+		
+		System.out.println(produto);
+
+	}
+
+}
